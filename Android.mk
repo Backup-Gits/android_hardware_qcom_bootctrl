717ecb6fc64425bab844bbfb3d727f83c43117c0
Chaosmaster <chaosmaster@chaosv.de>
2020-06-12 21:17:24 +0200

    Only use common bootctrl if device requests it
    This changes the default behavior to only build the common bootctrl
    if the device defines USE_COMMON_BOOTCTRL instead of having
    the device define BOARD_PROVIDES_BOOTCTRL to deselect it.
    
    Most devices currently provide their own, so don't use the common
    by default
    
    Change-Id: I29cc9db9b0d4a0b5d2fe60b021cc6e01363a8611

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(USE_COMMON_BOOTCTRL),true)
+
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 ifneq ($(filter msm8996 msm8998 sdm845,$(TARGET_BOARD_PLATFORM)),)
@@ -30,4 +32,6 @@ LOCAL_MODULE := bootctrl.$(TARGET_BOARD_PLATFORM)
 include $(BUILD_STATIC_LIBRARY)
 
 endif
+endif
+
 endif
